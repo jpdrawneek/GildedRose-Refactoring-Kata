01c663a8557d9045ea41afb4446cceaaf8145739
John-Paul Drawneek <jpdrawneek@users.noreply.github.com>
2020-11-15 18:54:15 +0000

    Cover described cases in tests

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -12,6 +12,10 @@ class Shop {
   }
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+      }
+      
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
@@ -35,9 +39,7 @@ class Shop {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
+
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -64,4 +66,4 @@ class Shop {
 module.exports = {
   Item,
   Shop
-}
+};
--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -6,4 +6,48 @@ describe("Gilded Rose", function() {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("foo");
   });
+
+  describe.each`
+  label | item | sellIn | quality | expectedSellIn | expectedQuality
+  ${'widget A'} | ${'widget A'} | ${10} | ${10} | ${9} | ${9}
+  ${'widget A out of date'} | ${'widget A'} | ${0} | ${10} | ${-1} | ${8}
+  ${'widget B'} | ${'widget B'} | ${10} | ${0} | ${9} | ${0}
+  ${'Aged Brie normal'} | ${'Aged Brie'} | ${10} | ${10} | ${9} | ${11}
+  ${'Aged Brie out of date'} | ${'Aged Brie'} | ${-1} | ${10} | ${-2} | ${12}
+  ${'Aged Brie max'} | ${'Aged Brie'} | ${10} | ${50} | ${9} | ${50}
+  ${'Sulfuras normal'} | ${'Sulfuras, Hand of Ragnaros'} | ${10} | ${10} | ${10} | ${10}
+  ${'Backstage passes'} | ${'Backstage passes to a TAFKAL80ETC concert'} | ${20} | ${10} | ${19} | ${11}
+  ${'Backstage passes 10 days'} | ${'Backstage passes to a TAFKAL80ETC concert'} | ${11} | ${10} | ${10} | ${12}
+  ${'Backstage passes 5 days'} | ${'Backstage passes to a TAFKAL80ETC concert'} | ${6} | ${10} | ${5} | ${13}
+  ${'Backstage passes 5 days max'} | ${'Backstage passes to a TAFKAL80ETC concert'} | ${6} | ${50} | ${5} | ${50}
+  ${'Backstage passes passed'} | ${'Backstage passes to a TAFKAL80ETC concert'} | ${0} | ${10} | ${-1} | ${0}
+  `('Standard item: "$label"', ({label, item, sellIn, quality, expectedSellIn, expectedQuality}) => {
+    let items;
+
+    beforeEach(() =>{
+      const gildedRose = new Shop([new Item(item, sellIn, quality)]);
+      items = gildedRose.updateQuality();
+    });
+
+    it('Should only be one item', () => {
+      expect(items).toHaveLength(1);
+    });
+
+    it('Item sell-in value should be updated correctly', () => {
+      expect(items[items.length - 1].sellIn).toEqual(expectedSellIn);
+    });
+
+    it('Item quality value should be updated correctly', () => {
+      expect(items[items.length - 1].quality).toEqual(expectedQuality);
+    });
+  });
+
+  describe.each`
+  label | item | sellIn | quality | expectedSellIn | expectedQuality
+  ${'widget A'} | ${'widget A'} | ${10} | ${10} | ${9} | ${9}
+  ${'widget B'} | ${'widget B'} | ${10} | ${0} | ${9} | ${0}
+  ${'Aged Brie normal'} | ${'Aged Brie'} | ${10} | ${10} | ${9} | ${11}
+  ${'Aged Brie max'} | ${'Aged Brie'} | ${10} | ${50} | ${9} | ${50}
+  ${'Sulfuras normal'} | ${'Sulfuras, Hand of Ragnaros'} | ${10} | ${10} | ${10} | ${10}
+  `('Conjured items: $label', ({label, item, sellIn, quality, expectedSellIn, expectedQuality}) => {});
 });
